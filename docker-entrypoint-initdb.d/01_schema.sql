daf429dd02bb080112de68fd92d655eb18a2993c
egorlichenkoam <egorlichenkoam@gmail.com>
2021-02-24 12:23:24 +0500

    ddl, dml, init on create

--- docker-entrypoint-initdb.d/01_schema.sql
+++ docker-entrypoint-initdb.d/01_schema.sql
@@ -20,7 +20,13 @@ CREATE TABLE cards
     owner_id BIGINT    NOT NULL REFERENCES clients,
     status   TEXT      NOT NULL DEFAULT 'INACTIVE' CHECK (status in ('INACTIVE', 'ACTIVE')),
     created  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
+);
+
+CREATE TABLE icons
+(
+    id  BIGSERIAL PRIMARY KEY,
+    url TEXT NOT NULL
+);
 
 CREATE TABLE transactions
 (
@@ -33,10 +39,6 @@ CREATE TABLE transactions
     icon_id  BIGINT    NOT NULL REFERENCES icons,
     status   TEXT      NOT NULL DEFAULT 'PROCESS' CHECK (status in ('PROCESS', 'EXECUTED')),
     created  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
+);
 
-CREATE TABLE icons
-(
-    id  BIGSERIAL PRIMARY KEY,
-    url TEXT NOT NULL
-)
+COMMIT;
